f35ae7ad6cb5a976492b246605ead66aa1e08754
Chen A Chen <chen.a.chen@intel.com>
2019-03-21 12:23:25 +0800

    ParserLibuCode.py: Code refine

--- ParserLibuCode.py
+++ ParserLibuCode.py
@@ -1,35 +1,25 @@
 from ParserLib import *
 
-class Microcode:
+class Microcode(ParserLib):
   def __init__(self, Payload):
-    self.__Payload = Payload
-    self.__FirstuCode = False
-    self.__Prefix = {
-      "L1":"|",
-      "L2":"| |",
-      "L3":"|   |",
-    }
-    self.__Relation = {
-      "L1":"|",
-      "L2":"|\ ",
-      "L3":"|  \ ",
-    }
+    ParserLib.__init__(self, Payload)
+    self.__IsFirstOutput = True
 
-  def __Sum32(self, Data, Len):
-    if Len % 4 != 0:
-      assert (False)
+  def __Sum32(self, Len):
+    if Len%4 != 0:
+      return -1
     Sum32 = 0
-    for Offset in range(0,Len,4):
-      Sum32 += ParseBaseType(UINT32, Data[Offset:Offset+4])
+    for Offset in range(0, Len, 4):
+      Sum32 += ParseBaseType(UINT32, self._Payload[self._CurOffset + Offset : self._CurOffset + Offset + 4])
       if Sum32 > 4294967295:
         Sum32 -= 4294967296
     return Sum32
 
-  def __IsMicrocodeValid(self, Data):
-    if len(Data) < 48:
+  def __IsHeaderValid(self):
+    if len(self._Payload[self._CurOffset:]) < 48:
       return False
 
-    Header = ParseStruct(CPU_MICROCODE_HEADER, Data)
+    Header = ParseStruct(CPU_MICROCODE_HEADER, self._Payload[self._CurOffset:])
 
     if Header[0] != 1:
       return False
@@ -37,10 +27,10 @@ class Microcode:
     if Header[5] != 1:
       return False
 
-    if Header[8] % 4 != 0 and len(Data) < Header[8]:
+    if Header[8] % 4 != 0 and len(self._Payload[self._CurOffset:]) < Header[8]:
       return False
 
-    if self.__Sum32(Data, int(Header[8])) != 0:
+    if self.__Sum32(Header[8]) != 0:
       return False
 
     if Header[7] != 0 and Header[8] < 48:
@@ -51,81 +41,91 @@ class Microcode:
 
     return True
 
-  def __ParseMircrocode(self, Offset):
-    if not self.__IsMicrocodeValid(self.__Payload[Offset:]):
-      return None
+  def __MainHeader(self):
+    Header    = ParseStruct(CPU_MICROCODE_HEADER, self._Payload[self._CurOffset:])
+    Prefix    = self._PrefixFormat(self._PrefixLevel)
+    Relation  = self._PrefixRelation(self._PrefixLevel)
 
-    #
-    # Main Header
-    #
-    self.__ParseMainHeader(Offset)
+    if self.__IsFirstOutput:
+      self.__IsFirstOutput = False
+    else:
+      print ("%s" % (Prefix))
 
-    Header = ParseStruct(CPU_MICROCODE_HEADER, self.__Payload[Offset:])
-    #
-    # No more data for Extended part.
-    #
+    print ("%sMicrocode Header:" % (Prefix), end = "")
+    print (" (Payload Offset = 0x%x)" % (self._CurOffset))
+    print ("%s  HeaderVersion      - 0x%x" % (Prefix, Header[0]))
+    print ("%s  UpdateRevision     - 0x%x" % (Prefix, Header[1]))
+    print ("%s  ProcessorSignature - 0x%x" % (Prefix, Header[3]))
+    print ("%s  Checksum           - 0x%x" % (Prefix, Header[4]))
+    print ("%s  LoaderRevision     - 0x%x" % (Prefix, Header[5]))
+    print ("%s  ProcessorFlags     - 0x%x" % (Prefix, Header[6]))
+    print ("%s  DataSize           - 0x%x" % (Prefix, Header[7]))
+    print ("%s  TotalSize          - 0x%x" % (Prefix, Header[8]))
+    self._CurOffset += StructLen(CPU_MICROCODE_HEADER)
+    self._CurOffset += Header[7]
+
     if 48 + Header[7] == Header[8]:
-      return
+      return False
     else:
-      Offset += StructLen(CPU_MICROCODE_HEADER) + Header[7]
+      return True
 
-    #
-    # Extended Table Header
-    #
-    Count = self.__ParseExtendedTableHeader(Offset)
-    Offset += StructLen(CPU_MICROCODE_EXTENDED_TABLE_HEADER)
+  def __ExtendedTableEntry(self, Index):
+    self._PrefixLevel += 1
+    Prefix    = self._PrefixFormat(self._PrefixLevel)
+    Relation  = self._PrefixRelation(self._PrefixLevel)
 
-    #
-    # Extended Table Entry
-    #
-    self.__ParseExtendedTableEntry(Offset, Count)
-
-  def __ParseMainHeader(self, Offset):
-    if self.__FirstuCode:
-      self.__FirstuCode = False
+    if Index == 0:
+      print ("%s" % (Relation))
     else:
-      print ("%s" % (self.__Relation["L1"]))
+      print ("%s" % (Prefix))
 
-    Header = ParseStruct(CPU_MICROCODE_HEADER, self.__Payload[Offset:])
-    print ("%sMicrocode Header:" % (self.__Prefix["L1"]), end = "")
-    print (" (Payload Offset = 0x%x)" % (Offset))
-    print ("%s  HeaderVersion      - 0x%x" % (self.__Prefix["L1"], Header[0]))
-    print ("%s  UpdateRevision     - 0x%x" % (self.__Prefix["L1"], Header[1]))
-    print ("%s  ProcessorSignature - 0x%x" % (self.__Prefix["L1"], Header[3]))
-    print ("%s  Checksum           - 0x%x" % (self.__Prefix["L1"], Header[4]))
-    print ("%s  LoaderRevision     - 0x%x" % (self.__Prefix["L1"], Header[5]))
-    print ("%s  ProcessorFlags     - 0x%x" % (self.__Prefix["L1"], Header[6]))
-    print ("%s  DataSize           - 0x%x" % (self.__Prefix["L1"], Header[7]))
-    print ("%s  TotalSize          - 0x%x" % (self.__Prefix["L1"], Header[8]))
+    Table = ParseStruct(CPU_MICROCODE_EXTENDED_TABLE, self._Payload[self._CurOffset:])
+    print ("%sMicrocode Extended Table:"    % (Prefix), end = "")
+    print (" (Payload Offset = 0x%x)"       % (self._CurOffset))
+    print ("%s  ProcessorSignature - 0x%x"  % (Prefix, Table[0]))
+    print ("%s  ProcessorFlags     - 0x%x"  % (Prefix, Table[1]))
+    print ("%s  Checksum           - 0x%x"  % (Prefix, Table[2]))
+    self._CurOffset += StructLen(CPU_MICROCODE_EXTENDED_TABLE)
 
-  def __ParseExtendedTableHeader(self, Offset):
-    print ("%s" % (self.__Relation["L2"]))
-    Header = ParseStruct(CPU_MICROCODE_EXTENDED_TABLE_HEADER, self.__Payload[Offset:])
-    print ("%sMicrocode Extended Table Header:" % (self.__Prefix["L2"]), end = "")
-    print (" (Payload Offset = 0x%x)" % (Offset))
-    print ("%s  ExtendedSignatureCount - 0x%x" % (self.__Prefix["L2"], Header[0]))
-    print ("%s  ExtendedChecksum       - 0x%x" % (self.__Prefix["L2"], Header[1]))
-    return Header[0]
+    self._PrefixLevel -= 1
 
-  def __ParseExtendedTableEntry(self, Offset, Count):
-    print ("%s" % (self.__Relation["L3"]))
-    for index in range(Count):
-      if index > 0:
-        print ("%s" % (self.__Prefix["L3"]))
-      Table = ParseStruct(CPU_MICROCODE_EXTENDED_TABLE, self.__Payload[Offset:])
-      print ("%sMicrocode Extended Table:" % (self.__Prefix["L3"]), end = "")
-      print (" (Payload Offset = 0x%x)" % (Offset))
-      print ("%s  ProcessorSignature - 0x%x" % (self.__Prefix["L3"], Table[0]))
-      print ("%s  ProcessorFlags     - 0x%x" % (self.__Prefix["L3"], Table[1]))
-      print ("%s  Checksum           - 0x%x" % (self.__Prefix["L3"], Table[2]))
-      Offset = Offset + StructLen(CPU_MICROCODE_EXTENDED_TABLE)
+  def __Extended(self):
+    self._PrefixLevel += 1
+    Prefix    = self._PrefixFormat(self._PrefixLevel)
+    Relation  = self._PrefixRelation(self._PrefixLevel)
 
-  def Dump(self):
-    Offset = 0
-    self.__FirstuCode = True
-    while Offset < len(self.__Payload):
-      Ret = self.__ParseMircrocode(Offset)
-      if Ret == None:
-        Offset += 1024
-      else:
-        Offset += Ret
+    Header = ParseStruct(CPU_MICROCODE_EXTENDED_TABLE_HEADER, self._Payload[self._CurOffset:])
+
+    print ("%s" % (Relation))
+    print ("%sMicrocode Extended Table Header:" % (Prefix), end = "")
+    print (" (Payload Offset = 0x%x)"           % (self._CurOffset))
+    print ("%s  ExtendedSignatureCount - 0x%x"  % (Prefix, Header[0]))
+    print ("%s  ExtendedChecksum       - 0x%x"  % (Prefix, Header[1]))
+
+    self._CurOffset += StructLen(CPU_MICROCODE_EXTENDED_TABLE_HEADER)
+
+    for index in range(Header[0]):
+      self.__ExtendedTableEntry(index)
+
+    self._PrefixLevel -= 1
+
+  def __Mircrocode(self):
+    if not self.__IsHeaderValid():
+      return False
+
+    if self.__MainHeader():
+      self.__Extended()
+    return True
+
+  def SetFirstOutput(self, First):
+    self.__IsFirstOutput = First
+
+  def DumpOne(self):
+    self._CurOffset = self._BegOffset
+    self.__Mircrocode()
+
+  def DumpAll(self):
+    self._CurOffset = self._BegOffset
+    while self._CurOffset < len(self._Payload):
+      if not self.__Mircrocode():
+        self._CurOffset += 1024
